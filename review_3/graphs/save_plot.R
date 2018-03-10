f998559502e3145e36674859d6a6fd9fe4aad15d
Adhithyan V <aavispeaks@gmail.com>
2018-03-10 13:59:18 +0530

    historical price plotted and beta calculated

---
+++ review_3/graphs/save_plot.R
@@ -0,0 +1,18 @@
+require(ggplot2)
+files = c("sbt.csv", "sbi.csv", "sbbj.csv", "sbm.csv")
+for (f in files) {
+  data <- read.csv(f)
+  min <- min(data$Close.Price)
+  max <- max(data$Close.Price)
+  
+  bank_name <- as.character(strsplit(f, ".csv"))
+  save_png <- paste("plots/", bank_name, ".png")
+  d <- as.Date(data$Date)
+  xrange <- as.Date(c(d[1], d[length(d)]))
+  png(filename=save_png)
+  plot(data$Close.Price ~ d, type="l", col="blue", ylim=c(min, max),
+       xlab = "Date", ylab = "INR", xlim=xrange)
+  title(main = paste(bank_name, "share price"), col.main="red", font.main=4)
+  dev.off()
+}
+
