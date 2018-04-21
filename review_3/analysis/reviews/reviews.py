b90188a8d661a00b8fe4e22975681a20dccb37a8
v-adhithyan <aavispeaks@gmail.com>
2018-04-21 11:24:29 +0530

    reviews scraped from mouthshut

--- review_3/analysis/reviews/reviews.py
+++ review_3/analysis/reviews/reviews.py
@@ -11,7 +11,17 @@ next_element = "next"
 urls = [
 "https://www.mouthshut.com/product-reviews/State-Bank-Of-Travancore-reviews-925004518",
 "https://www.mouthshut.com/product-reviews/State-Bank-Of-Travancore-reviews-925004518-page-2",
-"https://www.mouthshut.com/product-reviews/State-Bank-Of-Travancore-reviews-925004518-page-3"]
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Travancore-reviews-925004518-page-3",
+"https://www.mouthshut.com/product-reviews/State-Bank-of-Patiala-reviews-925106556",
+"https://www.mouthshut.com/product-reviews/State-Bank-of-Patiala-reviews-925106556-page-2",
+"https://www.mouthshut.com/product-reviews/State-Bank-of-Patiala-reviews-925106556-page-3",
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Hyderabad-reviews-925004478",
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Hyderabad-reviews-925004478-page-2",
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Hyderabad-reviews-925004478-page-3",
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Mysore-reviews-925004516",
+"https://www.mouthshut.com/product-reviews/State-Bank-of-Bikaner-and-Jaipur-reviews-925754283",
+"https://www.mouthshut.com/product-reviews/State-Bank-of-Bikaner-and-Jaipur-reviews-925754283-page-2"
+        ]
 
 to_csv = []
 fields = ["reviews"]
@@ -29,18 +39,22 @@ for url in urls:
 
     #print next.find("a").get("href")
     for review in reviews:
-        r = review.get_text().encode("ascii",  "ignore")
-        r = r.replace("...Read More", "")
-        #print r
-        row = {}
-        row[fields[0]] = r
-        to_csv.append(row)
+        try:
+            r = review.get_text().encode("ascii",  "ignore")
+            r = r.replace("...Read More", "")
+            #print r
+            row = {}
+            row[fields[0]] = r
+            to_csv.append(row)
+        except:
+            print "exception"
+        finally:
 
 
 time.sleep(5)
 
 print to_csv
-with open("sbtreviews.csv", "w") as csvfile:
+with open("reviews.csv", "w") as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fields)
     writer.writeheader()
     writer.writerows(to_csv)
---
+++ review_3/analysis/reviews/selenium_scraping_reviews.py
@@ -0,0 +1,82 @@
+from selenium import webdriver
+import time
+import csv
+import random
+
+browser = webdriver.Firefox()
+
+
+urls = [
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Travancore-reviews-925004518",
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Travancore-reviews-925004518-page-2",
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Travancore-reviews-925004518-page-3",
+"https://www.mouthshut.com/product-reviews/State-Bank-of-Patiala-reviews-925106556",
+"https://www.mouthshut.com/product-reviews/State-Bank-of-Patiala-reviews-925106556-page-2",
+"https://www.mouthshut.com/product-reviews/State-Bank-of-Patiala-reviews-925106556-page-3",
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Hyderabad-reviews-925004478",
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Hyderabad-reviews-925004478-page-2",
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Hyderabad-reviews-925004478-page-3",
+"https://www.mouthshut.com/product-reviews/State-Bank-Of-Mysore-reviews-925004516",
+"https://www.mouthshut.com/product-reviews/State-Bank-of-Bikaner-and-Jaipur-reviews-925754283",
+"https://www.mouthshut.com/product-reviews/State-Bank-of-Bikaner-and-Jaipur-reviews-925754283-page-2"
+    ]
+
+fields = ["date", "reviews", "star"]
+data = []    
+for url in urls:
+    browser.get(url)
+    
+    reviews = browser.find_elements_by_xpath("//div[@class='more reviewdata']")
+    dates = browser.find_elements_by_xpath("//p[@class='rating']")
+    read_more = browser.find_elements_by_xpath("//a[contains(text(), 'Read More')]")
+    
+    #print dir(dates[0])
+    #print len(dates[0].find_elements_by_class_name('rated-star'))
+    #print len(dates[0].find_elements_by_xpath("//i[@class='icon-rating rated-star']"))
+    
+    #expand all reviews
+    for r in read_more:
+        r.click()
+        time.sleep(random.randint(2, 4))
+    
+    i = 0
+    for review in reviews:
+        try:
+            text = review.text
+            text = text.encode("ascii", "ignore")
+            text = text.replace("...", "")
+            text = text.replace("Flag this review", "").lstrip().rstrip().strip()
+            date = dates[i].text
+            if "AM" in date:
+                date = date.split("AM")[0]
+            elif "PM" in date:
+                date = date.split("PM")[0]
+        
+            rating = str(len(dates[i].find_elements_by_class_name('rated-star')))
+            print text
+            print date
+            print rating
+        
+            row = {}
+            row[fields[0]] = date
+            row[fields[1]] = text
+            row[fields[2]] = rating
+            data.append(row)
+        except:
+            print "Exception"
+        finally:
+            print "in finally increment i"
+            i = i + 1
+    
+    print "Scraped a page.. going to sleep"
+    time.sleep(random.randint(10, 15))    
+    print "Wake from sleep"
+    
+browser.quit()
+
+print "writing to file"
+with open("reviews.csv", "w") as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fields)
+    writer.writeheader()
+    writer.writerows(data)
+print "Write success .."
