f7ea24edce521cff34946e467243b53e5bde36bd
v-adhithyan <aavispeaks@gmail.com>
2018-04-26 08:24:16 +0530

    rough draft 1 completed

--- review_3/analysis/reviews/ratings.R
+++ review_3/analysis/reviews/ratings.R
@@ -12,7 +12,7 @@ df <- data.frame(
   ratings = c(1:5),
   frequencies = c(71, 31, 23, 38, 36)
 )
-
+# 35.6, 15.6, 11.5, 19.1, 18.2
 png("ratings_distribution.png", width=300,height=350)
 gg <- ggplot(data=df, aes(x=ratings, y=frequencies, fill=as.factor(ratings))) + geom_bar(stat="identity")
 gg <- gg + scale_fill_brewer(palette = "Dark2") + guides(fill=FALSE)
